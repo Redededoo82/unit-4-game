e2f37f84696ae87b67f534b084a411d3062e5da0
AARON M <asmarcks@gmail.com>
2019-09-10 20:04:37 -0500

    lskjflksdjflksdjflksdjf

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// add static elements to the screen
+
+// generate random numbers for each  crystal and the goal
+
+// assign those random numbers to the crystals
+
+// display the goal number on the screen
+
+//on the click of a crystal 
+    // grab that crystals value
+    // add that value to the total
+        // if the total is greater than the goal
+            // display "you lose"  *bonus reset the game
+        // else if the total equals the goal "you win"
+        
